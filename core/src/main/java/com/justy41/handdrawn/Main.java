ab0411e25b3ff42240b2d3c099386934ff2f2dfb
Babiciu Iustin <167506994+justy41@users.noreply.github.com>
2026-01-14 23:31:24 +0200

    Initial commit

---
+++ core/src/main/java/com/justy41/handdrawn/Main.java
@@ -0,0 +1,35 @@
+package com.justy41.handdrawn;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Main extends ApplicationAdapter {
+    private SpriteBatch batch;
+    private Texture image;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        image = new Texture("libgdx.png");
+    }
+
+    @Override
+    public void render() {
+        // UPDATE
+
+        // RENDER
+        ScreenUtils.clear(0.21f, 0.15f, 0.2f, 1f);
+        batch.begin();
+        batch.draw(image, 140, 210);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        image.dispose();
+    }
+}
